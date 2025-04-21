da6d473e4e8ed51106ab8da015e45a0cb9edffe4
Duta <dhuta.azikiras@gmail.com>
2025-04-21 17:07:08 +0700

    Added CA ceritiface for Aieven cloud database

--- db.py
+++ db.py
@@ -9,7 +9,14 @@ load_dotenv()
 # === Database Connection ===
 DATABASE_URL = "mysql://avnadmin:AVNS_A9ZtoDM9GQ7x54SdoYw@hospital-chatbot-service-hospital-chatbot-management.k.aivencloud.com:27086/defaultdb?ssl-mode=REQUIRED"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={
+        "ssl_ca": "/certs/ca.pem",  # Full path to your CA cert
+        "ssl_verify_cert": True         # Enforce SSL certificate validation
+    }
+)
 
 def get_session():
     with Session(engine) as session:
